88adfdfc675c6f9a189bc45a927d6bbb9a25ba2a
Mr.javad <javadmohamadi70001377@gmail.com>
2017-06-23 11:03:22 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -97,11 +97,11 @@ function create_config( )
     info_text = [[》SeniorBot v6.0
 An advanced administration bot based on https://valtman.name/telegram-cli
 》https://github.com/javad7070/sn 
-》Admins :
+》Admins:
 》@Cpp_cs 
-》Our channel :
+》Our channel:
 》@SeniorTm《
-》Our website :
+》Our website:
 》https://seniorbot.ml/
 ]],
   }
